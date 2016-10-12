0112eb33e0eb680f9ca43832d7307698fea9a9dd
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-10-12 14:29:42 -0400

    Work in Progress: Adding EF and initial model/context

---
+++ MVC-EF-CRUD/MVC-EF-CRUD/Models/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_EF_CRUD.Models
+{
+    //Normally broken down in different files.  Planet.CS etc. 
+
+    public class Planet
+    {
+        public int PlanetID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PlanetContext : DbContext
+    {
+        public DbSet<Planet> Planets { get; set; }
+    }
+}
